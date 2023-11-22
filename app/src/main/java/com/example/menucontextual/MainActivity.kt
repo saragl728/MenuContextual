a1d4e559ef5540f2487395e7b077ff738fb707ca
Usuario <saragl728@gmail.com>
2023-11-22 08:55:44 +0100

    Implemento códigos y renombro un archivo

--- app/src/main/java/com/example/menucontextual/MainActivity.kt
+++ app/src/main/java/com/example/menucontextual/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.menucontextual
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.example.menucontextual.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val imagen = binding.imagenUsuario
+        registerForContextMenu(imagen)
+
+        fun onCreateContextMenu(
+            menu: ContextMenu?,
+            v: View?,
+            menuInfo: ContextMenu.ContextMenuInfo?
+        ) {
+            super.onCreateContextMenu(menu, v, menuInfo)
+            menuInflater.inflate(R.menu.menu_contextual, menu)
+            menu?.setHeaderTitle("Menú contextual")
+        }
+
+        fun onContextItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId){
+                R.id.descargar -> {
+                    Toast.makeText(this, "Opción descargar", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.compartir -> {
+                    Toast.makeText(this, "Opción compartir", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.copiar -> {
+                    Toast.makeText(this, "Opción copiar", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                else -> {
+                    super.onContextItemSelected(item)
+                }
+            }
+        }
     }
 }
