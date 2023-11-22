70bb7be947c47cde535ebb7ecff87ce016f3114d
Usuario <saragl728@gmail.com>
2023-11-22 08:35:55 +0100

    Habilito el binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.menucontextual"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.menucontextual"
         minSdk = 24
